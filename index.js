e505dd9ea72b306c13149911ec6e043ae5807ddf
kayjayy22 <karamjeet.kamboj22@gmail.com>
2025-10-16 01:36:54 +0530

    feat: Support custom delimiters

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ export const Add = (numbers) =>{
 	if (numbers === '') {
 		return 0;
 	}
-	const nums = numbers.split(/[,\n]/);
+	
+	let delimiter = /[,\n]/;
+	let numString = numbers;
+	
+	if (numbers.startsWith('//')) {
+		const parts = numbers.split('\n');
+		const customDelimiter = parts[0].substring(2);
+		delimiter = new RegExp(`[,\n${customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}]`);
+		numString = parts[1];
+	}
+	
+	const nums = numString.split(delimiter);
 	return nums.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }

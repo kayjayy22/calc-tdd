eb74a0f3777489e7f12f606b3baa31491944f5b0
kayjayy22 <karamjeet.kamboj22@gmail.com>
2025-10-16 01:23:12 +0530

    test: Add failing test for empty string

--- index.test.js
+++ index.test.js
@@ -3,6 +3,6 @@ import { Add } from './index';
 
 describe('String Calculator', () => {
   it('should return 0 for an empty string', () => {
-    expect(Add('')).toBe(0);
+    expect(Add("")).toBe(0);
   });
 });

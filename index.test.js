365fb0ea66e57764c363e8737f98309391d78066
kayjayy22 <karamjeet.kamboj22@gmail.com>
2025-10-16 01:27:14 +0530

    test: Add failing test for single number

--- index.test.js
+++ index.test.js
@@ -5,4 +5,8 @@ describe('String Calculator', () => {
   it('should return 0 for an empty string', () => {
     expect(Add("")).toBe(0);
   });
+
+  it('should return the number when the string contains a single number', () => {
+    expect(Add("1")).toBe(1);
+  });
 });

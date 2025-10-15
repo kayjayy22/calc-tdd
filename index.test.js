e8e06da5c145098fa66b4bfe33b30f7f56656559
kayjayy22 <karamjeet.kamboj22@gmail.com>
2025-10-15 15:19:48 +0530

    build: Project setup

---
+++ index.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import { Add } from './index';
+
+describe('String Calculator', () => {
+  it('should return 0 for an empty string', () => {
+    expect(Add('')).toBe(0);
+  });
+});
